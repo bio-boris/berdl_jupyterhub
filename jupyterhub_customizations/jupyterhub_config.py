60d0ef03ddf558ac57efb69df97017b8aa89721b
bio-boris <bio-boris@github.com>
2025-07-22 11:52:00 -0500

    Update envs

--- jupyterhub_customizations/jupyterhub_config.py
+++ jupyterhub_customizations/jupyterhub_config.py
@@ -1,4 +1,4 @@
-from jupyterhub_customizations.kb_jupyterhub_auth import KBaseAuthenticator, kbase_origin
+from jupyterhub_customizations.kb_jupyterhub_auth import KBaseAuthenticator
 import os
 
 c = get_config()
