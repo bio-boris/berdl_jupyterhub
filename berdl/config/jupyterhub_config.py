b9dc0f1c07158f60a57c73035a151d3d3a00a156
bio-boris <bio-boris@github.com>
2025-07-24 16:41:53 -0500

    Use jupyterstacks

--- berdl/config/jupyterhub_config.py
+++ berdl/config/jupyterhub_config.py
@@ -111,32 +111,35 @@ c.KubeSpawner.profile_list = [
     {
         "display_name": "Small Server (2G RAM, 1 CPU)",
         "default": True,
-        "image": "jupyter/base-notebook:latest",
+
         "kubespawner_override": {
             "mem_limit": "2G",
             "mem_guarantee": "1G",
             "cpu_limit": 1,
             "cpu_guarantee": 0.5,
+            "image": "jupyter/base-notebook:latest",
         },
     },
     {
         "display_name": "Medium Server (8G RAM, 2 CPU) w quay.io/jupyter/pyspark-notebook:spark-4.0.0",
-        "image": "quay.io/jupyter/pyspark-notebook:spark-4.0.0",
+
         "kubespawner_override": {
             "mem_limit": "8G",
             "mem_guarantee": "4G",
             "cpu_limit": 2,
             "cpu_guarantee": 1,
+            "image": "quay.io/jupyter/pyspark-notebook:spark-4.0.0",
         },
     },
     {
-        "display_name": f"Large Server (32G RAM, 4 CPU) with f{berdl_notebook_image_tag}",
-        "image": f"{berdl_notebook_image_tag}",
+        "display_name": f"Large Server (32G RAM, 4 CPU) with {berdl_notebook_image_tag}",
+
         "kubespawner_override": {
             "mem_limit": "32G",
             "mem_guarantee": "16G",
             "cpu_limit": 4,
             "cpu_guarantee": 2,
+            "image": f"{berdl_notebook_image_tag}",
         },
     },
 ]
